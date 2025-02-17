572e391d320364a6df129e0b8171a45130fd46f8
Raúl Vásquez <rs.vasquezl@uea.edu.ec>
2025-02-16 21:59:53 -0500

    Trabajo Semana 9

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+
+# Ruta principal
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world(): # Colocar aqui el codigo principal
+    return 'Practica de Pagina Web!'
+
+# Ruta personalizada con usuario
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'<h1>¡Bienvenido, {nombre}!</h1><p>Tu usuario ha sido registrado correctamente.</p>'
+
 
 if __name__ == '__main__':
     app.run()
